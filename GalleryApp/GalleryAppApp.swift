6001b884fc9f8b0ab944798708948f4a5caf2ec1
Anand.Narayan <arnarayan@gmail.com>
2022-07-01 09:40:49 -0600

    Initial Commit

---
+++ GalleryApp/GalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryAppApp.swift
+//  GalleryApp
+//
+//  Created by Anand Narayan on 2022-07-01.
+//
+
+import SwiftUI
+
+@main
+struct GalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
